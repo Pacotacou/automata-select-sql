44018a0fcf8d340e4d3de211e31504eb1934fbbd
Pacotacou <pacomoises01@gmail.com>
2023-10-08 11:49:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,36 +2,56 @@ import tkinter as tk
 from automatapy import isValidSql
 
 MENSAJE_LABEL ='-La sentencia FROM es obligatoria\n'
-MENSAJE_LABEL+='-No se contemplan SUBCONSULTAS, NI RELACIONES AVANZADAS (JOIN)\n'
-MENSAJE_LABEL+='-NO se contemplan operaciones con PARÉNTESIS'
+MENSAJE_LABEL+='-No se contemplan SUBCONSULTAS\n'
+MENSAJE_LABEL+='-No se contemplan RELACIONES AVANZADAS (JOIN)\n'
+MENSAJE_LABEL+='-No se contemplan operaciones con PARÉNTESIS'
+
+FONT = "Verdana"
 
 class App(tk.Frame):
     def __init__(self, master):
         #widgets
         super().__init__(master)
         self.lblInstruccion = tk.Label(text="Ingrese su consulta SQL:",
-                                       font=('Consolas',12,'bold'))
+                                       font=(FONT,12,'bold'))
         self.lblMensaje = tk.Label(text=MENSAJE_LABEL,justify='left',
-                                   font=('Consolas',8,''),
+                                   font=(FONT,8,''),
                                    fg='gray')
         self.txtConsultaSQL = tk.Text(height=7,width=40,
-                                      font=('Consolas',12,''))
-        self.btnComprobar = tk.Button(text="VERIFICAR",
-                                      font=('Consolas',16,'bold'))
+                                      font=(FONT,12,''))
+        self.ContainerBtn = tk.Frame(self)
+        self.btnComprobar = tk.Button(self.ContainerBtn,text="VERIFICAR",
+                                      font=(FONT,14,''),
+                                      width=15,
+                                      fg='black')
+        self.btnBorrar = tk.Button(self.ContainerBtn,text="BORRAR",
+                                      font=(FONT,14,''),
+                                      width=15,
+                                      fg='black')
         self.lblResultado = tk.Label(text="(resultado...)",
-                                     font=('Consolas',12,'bold'))
+                                     font=(FONT,12,'bold'),
+                                     fg='gray')
         
         self.lblInstruccion.pack(pady=5)
-        self.lblMensaje.pack(pady=5)
+        self.lblMensaje.pack(pady=5,fill='x')
         self.txtConsultaSQL.pack(padx=20,fill='both')
-        self.btnComprobar.pack(padx=10,pady=10,fill='x')
-        self.lblResultado.pack()
+        self.ContainerBtn.pack(pady=10,fill='x')
+        self.btnComprobar.grid(row=0, column=0, padx=4, pady=4, sticky='w')
+        self.btnBorrar.grid(row=0, column=1, padx=4, pady=4, sticky='e')
+        self.lblResultado.pack(pady=5)
         self.pack()
 
         #eventos
         self.btnComprobar.bind('<Button-1>',self.btnComprobarClic)
+        self.btnBorrar.bind('<Button-1>',self.btnBorrarClic)
     
     #funciones
+    def btnBorrarClic(self,event):
+        self.txtConsultaSQL.delete("1.0","end")
+        self.lblResultado.config(text="(resultado...)",
+                                     font=(FONT,12,'bold'),
+                                     fg='gray')
+
     def btnComprobarClic(self,event):
         cadena = self.txtConsultaSQL.get("1.0","end-1c").upper()
         isValid = isValidSql(cadena+" ")
