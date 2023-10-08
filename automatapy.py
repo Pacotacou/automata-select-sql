5fe939c85547db75b604cb3dac0e17366b00a328
Pacotacou <pacomoises01@gmail.com>
2023-10-08 12:54:54 -0500

    Update automatapy.py

--- automatapy.py
+++ automatapy.py
@@ -1,22 +1,31 @@
 import re
 
+#Función para verificar consultas SQL
 def isValidSql(str):
+    #Lista de palabras reservadas de SQL
     BlackList = ['WHERE','FROM','SELECT','JOIN',
-                 'INNER','FULL','AND','OR','NOT','LIKE']
+                 'INNER','FULL','AND','OR','NOT',
+                 'LIKE','DELETE','DROP','DELETE',
+                 'CREATE','UPDATE','ALTER']
+    
+    #String de negación para las palabras reservadas de SQL
     BlackString = "("+"\W)|(".join(BlackList)+"\W)"
     RES = f'(?!{BlackString})'
-    
+    #Variables
     TABLE = f'({RES}([A-Z](\_?([A-Z]|\d))*))'
     ATRIB = f'(({TABLE}(\.{TABLE})?))'
     VALUE = '((\d+(\.\d+)?)|(\'.*\'))'
     OP = '(\+|\-|\*|\/|\%|(\sAND\s)|(\sOR\s)|(\<\=?)|(\>\=?)|(\=\=)|(\!\=)|(\sLIKE\s))'
 
+    #Atributo o Valor, puede ir con cero, uno o varios NOT al principio
     ATRIBVAL =  f'((\s*NOT\s+)*({ATRIB}|{VALUE}))'
 
+    #Construcción de la expresión regular
     RegEx = f'\s*SELECT\s+(\*|{ATRIBVAL}(\s*\,\s*{ATRIBVAL})*)'
     RegEx += f'\s+FROM\s+{TABLE}(\s*,\s*{TABLE})*\s*'
     RegEx += f'(\s+WHERE\s+{ATRIBVAL}(\s*{OP}\s*{ATRIBVAL})*)?\s*'
 
+    #Retornar el resultado
     return re.fullmatch(RegEx,str)
 
     
