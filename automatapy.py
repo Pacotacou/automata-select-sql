0ee1257760cce63c2984393510a64f446efb462f
Pacotacou <pacomoises01@gmail.com>
2023-10-08 02:31:00 -0500

    commm

--- automatapy.py
+++ automatapy.py
@@ -3,19 +3,19 @@ import re
 def isValidSql(str):
     #RES = f'((?!WHERE\s+|FROM\s+|SELECT\s+|JOIN\s+|INNER\s+|FULL\s+))'
     BlackList = ['WHERE','FROM','SELECT','JOIN',
-                 'INNER','FULL','AND','OR','NOT']
+                 'INNER','FULL','AND','OR','NOT','LIKE']
     BlackString = "\s+|".join(BlackList)+"\s+"
     RES = f'((?!{BlackString}))'
     
     TABLE = f'{RES}\s*([A-Z](\_?([A-Z]|\d))*)'
     ATRIB = f'{RES}\s*({TABLE}(\.{TABLE})?)'
-    VALUE = "((\d+(\.\d+)?) | ('(\w|\s)*') )"
-    OP = '(\+|\-|\*|\/|\%|(\sAND\s)|(\sOR\s)|(\<=?)|(\>=?)|(\=\=))'
+    VALUE = '((\d+(\.\d+)?)|(\'.*\'))'
+    OP = '(\+|\-|\*|\/|\%|(\sAND\s)|(\sOR\s)|(\<=?)|(\>=?)|(\=\=)|(\sLIKE\s))'
 
     #ATRIBVAL = f'({ATRIB}|{VALUE})'
     ATRIBVAL =  f'((\s*NOT\s+)*({ATRIB}|{VALUE}))'
 
-    RegEx = f'SELECT\s+(\*|{ATRIB}(\s*\,\s*{ATRIB})*)'
+    RegEx = f'SELECT\s+(\*|{ATRIBVAL}(\s*\,\s*{ATRIBVAL})*)'
     RegEx += f'\s+FROM\s+{TABLE}(\s*,\s*{TABLE})*\s*'
     RegEx += f'(\s+WHERE\s+(NOT\s+)*{ATRIBVAL}(\s*{OP}\s*{ATRIBVAL})*)?\s*'
 
